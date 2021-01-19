7f3d423cdfce40ca75067e0d4b2efe94a68d61b7
Mohammad <m.moharrer@outlook.com>
2021-01-19 20:59:11 +0330

    startup comment

--- JobQueue/Program.cs
+++ JobQueue/Program.cs
@@ -6,7 +6,22 @@ namespace JobQueue
     {
         static void Main(string[] args)
         {
-            ColorConsole.WriteWarning("For stopping threads press 'e'");
+            ColorConsole.WriteWarning(
+@" *****************************************Senario*****************************************
+   * job list is in JobCollection.cs and Http call simulation int FakeHttpService.cs
+   * Express category: 
+   * HttpCall after occure after 5 seconds
+   * Default is  5 thread and can configurable
+   * Failure retry count is 3 and configurable
+   * job1 must fail after 3 time retry
+   * job 1 & job2 & job3 have same entity name then executing order is by creation time (job1, job2, job3)
+   * job 1 is depend on job9 
+   * job 2 is depend on job8
+   * job 3 is depend on job9
+   *****************************************************************************************"
+            );
+
+            ColorConsole.WriteLine("For stopping jobs press 'e'");
             ColorConsole.WriteLine("Press any key to Start jobs");
             Console.Read();
 
